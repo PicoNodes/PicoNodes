63f44a0c95dff0e70a3e659d38850b5121d75bf0
Teo Klestrup Röijezon <teo@nullable.se>
2018-04-19 16:31:50 +0200

    Implemented opendrainy in terms of the HAL

--- firmware/node-compute/opendrainy/src/main.rs
+++ firmware/node-compute/opendrainy/src/main.rs
@@ -7,36 +7,41 @@ extern crate cortex_m;
 extern crate cortex_m_rt;
 extern crate cortex_m_semihosting;
 extern crate panic_semihosting;
-extern crate stm32f0x0;
-
-use cortex_m::asm;
+extern crate stm32f0x0_hal;
+extern crate embedded_hal;
 
 use core::fmt::Write;
 use cortex_m_semihosting::hio;
 
-use stm32f0x0::Peripherals;
+use embedded_hal::digital::*;
+
+use stm32f0x0_hal::prelude::*;
+use stm32f0x0_hal::delay::Delay;
+use stm32f0x0_hal::stm32f0x0::{CorePeripherals, Peripherals};
 
 fn main() {
     let mut out = hio::hstdout().unwrap();
     let peripherals = Peripherals::take().unwrap();
-    let gpioa = peripherals.GPIOA;
-    let rcc = peripherals.RCC;
+    let core_peripherals = CorePeripherals::take().unwrap();
+    let mut flash = peripherals.FLASH.constrain();
+    let mut rcc = peripherals.RCC.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    rcc.ahbenr.modify(|_, w| w.iopaen().set_bit()); // enable IO port A clock
-    unsafe {
-        gpioa.otyper.modify(|_, w| w.ot4().set_bit()); // open drain
-        gpioa.moder.modify(|_, w| w.moder4().bits(1)); // output
-    }
+    let mut gpioa = peripherals.GPIOA.split(&mut rcc.ahb);
+    let mut delay = Delay::new(core_peripherals.SYST, clocks);
+
+    let mut pa4 = gpioa.pa4.into_open_drain_output(&mut gpioa.moder, &mut gpioa.otyper);
 
     let mut state = true;
     loop {
         state = !state;
-        gpioa.odr.modify(|_, w| w.odr4().bit(state));
-        writeln!(out, "O: {}, I: {}", gpioa.odr.read().odr4().bit(), gpioa.idr.read().idr4().bit()).unwrap();
-
-        // asm::bkpt();
-        for _i in 0..1000000 {
-            asm::nop();
+        if state {
+            pa4.set_high();
+        } else {
+            pa4.set_low();
         }
+        writeln!(out, "O: {}, I: {}", OutputPin::is_high(&pa4), InputPin::is_high(&pa4)).unwrap();
+
+        delay.delay_ms(500u16);
     }
 }
