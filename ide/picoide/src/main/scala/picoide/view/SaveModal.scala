dde596011499f647987f8f27f1403e0761a6ef32
Teo Klestrup Röijezon <teo@nullable.se>
2018-05-16 13:07:35 +0200

    Made the save prompt (slightly) prettier

--- ide/picoide/src/main/scala/picoide/view/SaveModal.scala
+++ ide/picoide/src/main/scala/picoide/view/SaveModal.scala
@@ -14,17 +14,24 @@ object SaveModal {
       .render_P(props =>
         <.div(<.div(
           ^.className := "save-modal",
-          <.h2("Danger!"),
           <.div(
-            "You will lose any unsaved data! Are you sure you want to continue?"),
-          <.button("Save",
-                   ^.onClick --> props.dispatchCB(Actions.CurrentFile.Save)),
-          <.button("Ignore",
-                   ^.onClick --> props.dispatchCB(
-                     Actions.CurrentFile.PromptSaveIgnore)),
-          <.button("Cancel",
-                   ^.onClick --> props.dispatchCB(
-                     Actions.CurrentFile.PromptSaveCancel)),
+            ^.className := "save-modal-content",
+            <.h2("Danger!"),
+            <.div("You will lose any unsaved data! Are you sure you want to continue?"),
+            <.div(
+              ^.className := "save-modal-button-row",
+              <.button(
+                "Save",
+                ^.onClick --> props.dispatchCB(Actions.CurrentFile.Save)),
+              <.button("Ignore",
+                       ^.onClick --> props.dispatchCB(
+                         Actions.CurrentFile.PromptSaveIgnore)),
+              Spacer.component(),
+              <.button("Cancel",
+                       ^.onClick --> props.dispatchCB(
+                         Actions.CurrentFile.PromptSaveCancel))
+            ),
+          )
         ).when(props().fold(false)(_.nextCleanAction.isDefined))))
       .build
 }
